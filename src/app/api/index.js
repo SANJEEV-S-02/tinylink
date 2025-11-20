33813e61f247f7478824585c04c15fad504e1b85
SANJEEV-S-02 <sanjeev@adhiba.com>
2025-11-20 19:28:31 +0530

    5001

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import { registerUser } from './auth/register.js';
-import { loginUser } from './auth/login.js';
-import linksRoute from './links/index.js';
-import redirectRoute from './links/redirect.js';
+import { registerUser } from "./auth/register.js";
+import { loginUser } from "./auth/login.js";
+import linksRoute from "./links/index.js";
+import redirectRoute from "./links/redirect.js";
 
 const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://tinylink01.vercel.app/'],
     methods: ['GET', 'POST', 'DELETE'],
     credentials: true,
   })
@@ -19,13 +19,13 @@ app.use(
 app.use(express.json());
 
 // AUTH
-app.post('/api/auth/register', registerUser);
-app.post('/api/auth/login', loginUser);
+app.post("/api/auth/register", registerUser);
+app.post("/api/auth/login", loginUser);
 
 // LINKS
-app.use('/api/links', linksRoute);
+app.use("/api/links", linksRoute);
 
 // REDIRECT
-app.use('/', redirectRoute);
+app.use("/", redirectRoute);
 
-app.listen(5001, () => console.log('API running at http://localhost:5001'));
+app.listen(5001, () => console.log("API running at http://localhost:5001"));
