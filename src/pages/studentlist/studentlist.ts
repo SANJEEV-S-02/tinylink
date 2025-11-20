fe6a6a713f7f44d5487f35e90ca9bb7d5e05bca8
SANJEEV-S-02 <sanjeev@adhiba.com>
2025-11-20 18:57:40 +0530

    Initial tinylink project push

--- src/pages/studentlist/studentlist.ts
+++ src/pages/studentlist/studentlist.ts
@@ -3,5 +3,7 @@ import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import PageMeta from "../../components/common/PageMeta";
 import BasicTableOne from "../../components/studentlist/studentlist";
 export default function Studentlist() {
+  
+  
     return (_jsxs(_Fragment, { children: [_jsx(PageMeta, { title: "Nystai Institute | CCTV & Home Automation Course Training", description: "Join Nystai Institute to master CCTV installation and home automation systems. Get hands-on training, expert guidance, and industry-ready skills for a successful tech career." }), _jsx(PageBreadcrumb, { pageTitle: "Your Short Links" }), _jsx(BasicTableOne, {})] }));
 }
